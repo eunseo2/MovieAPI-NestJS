22dfdc3b84f5ef73710c230fe6939273b4a343be
eunseo2 <20180972@sungshin.ac.kr>
2021-03-26 20:51:09 +0900

    db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 //앱 모듈 : 모든 것의 루트 모듈
 
 import { Module } from '@nestjs/common';
-
+import { Movie } from './movies/entities/movie.entity';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'test',
+      entities: [Movie],
+      synchronize: true,
+    }),
+  ],
   controllers: [MoviesController], // url을 가져오고 함수를 실행 (express의 router 같은)
   providers: [MoviesService],
 })
--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,7 +1,13 @@
-export class Movie {
-  id: number;
-  title: string;
-  year: number;
-  genres: string[];
-}
 //type orm 써야함
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+@Entity()
+export class Movie {
+  @PrimaryGeneratedColumn()
+  id?: number;
+  @Column({ nullable: false })
+  title: string;
+  @Column({ nullable: false })
+  year: number;
+  //@Column()
+  //genres: string[];
+}
